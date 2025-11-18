d56b66a2ec84a4491f93a573846bdfa182eeca4d
Richard Galdeano <galdeanr@etu.univ-grenoble-alpes.fr>
2025-11-18 18:19:05 +0100

    Beetle progress

--- gameplay/enemies/beetle.gd
+++ gameplay/enemies/beetle.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 
+var start_position: Vector2
+
+func _ready() -> void:
+	start_position = global_position
+
 func _on_detection_zone_body_entered(body: Node2D) -> void:
 	if body is Antonia:
 		print("Antonia entered!")
