cfea60c3333bc7886274d935b618a57869131f28
Blake Sawyer <blakepsawyer@gmail.com>
2025-11-18 21:05:10 -0800

    Add interaction prompts to interactibles in the world

--- gameplay/groundItems/resourcePickups/pickup.gd
+++ gameplay/groundItems/resourcePickups/pickup.gd
@@ -4,8 +4,18 @@ class_name Pickup extends Area2D
 @export var root: Node
 var id: int
 
+@onready var prompt = $"../PickupPrompt"
+
 func _ready() -> void:
 	id = IdManager.get_id()
+	area_entered.connect(show_interaction_prompt)
+	area_exited.connect(hide_interaction_prompt)
 
 func pickup() -> void:
 	root.queue_free()
+
+func show_interaction_prompt(_area: Area2D) -> void:
+	prompt.show()
+
+func hide_interaction_prompt(_area: Area2D) -> void:
+	prompt.hide()
